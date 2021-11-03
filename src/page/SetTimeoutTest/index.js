ffd9fb92c145f8e0902da4e0730d7118b798e4bb
ch <chenhuan1@akulaku.com>
2021-11-03 19:04:26 +0800

    feat: bigturntable

--- src/page/SetTimeoutTest/index.js
+++ src/page/SetTimeoutTest/index.js
@@ -1,24 +1,36 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // 保存新回调
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  // 建立 interval
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 function Counter() {
   let [count, setCount] = useState(0);
 
-//   useEffect(() => {
-//     console.log(11);
-//     let id = setInterval(() => {
-//       setCount(count + 1);
-//     }, 1000);
-//     return () => clearInterval(id);
-//   });
-
-  useEffect(() => {
-    let id = setInterval(() => {
-      setCount(count + 1);
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
+  useInterval(() => {
+    // 你自己的代码
+    setCount(count + 1);
+  }, 1000);
 
   return <h1>{count}</h1>;
 }
 
-export default Counter;
+
+export default Counter
+
